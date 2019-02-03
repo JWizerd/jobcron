4c209ee0492a4f9a221d362828f7c40311bf02db
JWizerd <jwodke@outlook.com>
2019-02-02 19:12:07 -0700

    commented out cron cmd in bootstrap so we're not implicitly sending emails or scrapin websites while working on other stuff

--- docker/dev/bootstrap.sh
+++ docker/dev/bootstrap.sh
@@ -8,8 +8,8 @@ echo "Launching Apache2 webserver...";
 cd /var/www/ && php composer.phar dumpautoload -o;
 
 echo "Launching the crontab....";
-/usr/sbin/cron -f &
-touch /var/www/html/cronErrors.txt
-chmod 0777 /var/www/html/cronErrors.txt
+# /usr/sbin/cron -f &
+# touch /var/www/html/cronErrors.txt
+# chmod 0777 /var/www/html/cronErrors.txt
 
 exec apache2-foreground

d944b24efee838dc73817f6d0e3a1e2f4d195626
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-10 12:48:45 -0700

    feat(Database Schema)add user class for user collection migration

--- src/Database/schema/User.php
+++ src/Database/schema/User.php
@@ -1,11 +1,17 @@
 <?php 
 namespace JobCron\Database\schema;
+use JobCron\Database\schema\Base;
 
 
-class User {
+class User extends Base{
+    function __construct() {
+       return  parent::__construct();
+    }
+
+    function collection_name() {
+        return "users";
+    }
     
 }
-
-
 
 ?>
